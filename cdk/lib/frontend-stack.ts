fc9e1d33bec9e36bc59140955cd30f5904e2d073
Binh Ngo <bnngo97@gmail.com>
2023-04-12 12:15:30 -0700

    minor edit

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -5,7 +5,6 @@ import { SPADeploy } from 'cdk-spa-deploy';
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     new SPADeploy(this, 'spaDeploy')
       .createBasicSite({
         indexDoc: 'index.html',
