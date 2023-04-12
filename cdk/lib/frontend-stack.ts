5638437ba02e0edb03430721be58b9b6c9a85fcb
Binh Ngo <bnngo97@gmail.com>
2023-04-12 13:05:06 -0700

    minor edit

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -5,16 +5,12 @@ import { SPADeploy } from 'cdk-spa-deploy';
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new SPADeploy(this, 'spaDeploy')
-      .createBasicSite({
-        indexDoc: 'index.html',
-        websiteFolder: '../blog/dist/blog'
-      });
 
     new SPADeploy(this, 'cfDeploy')
       .createSiteWithCloudfront({
         indexDoc: 'index.html',
-        websiteFolder: '../blog/dist/blog'
+        websiteFolder: '../build',
+        certificateARN: "arn:aws:cloudfront::539460444185:distribution/E2XL4ZXZHC16QU"
       });
   }
 }
