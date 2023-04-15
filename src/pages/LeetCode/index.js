b84438f4474d2c28485aac042e3962ee116dceca
Binh Ngo <bnngo97@gmail.com>
2023-04-15 15:11:01 -0700

    edited problem 20's description

--- src/pages/LeetCode/index.js
+++ src/pages/LeetCode/index.js
@@ -108,10 +108,8 @@ var longestCommonPrefix = function(strs) {
         },
         {
 title: "20. Valid Parentheses",
-description: `Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
+description: `Given a string s containing just the characters (  ,  )  ,  {  ,  }  ,  [  , and  ]  , determine if the input string is valid.
 An input string is valid if:
-
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.`,
